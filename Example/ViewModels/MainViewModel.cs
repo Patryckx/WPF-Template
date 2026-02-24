050e2d4d880e5ace6e1b47ffe1ab5c23726d3e7d
ewin-csx <edwin.castillo@smitech.com.mx>
2026-02-24 15:57:04 -0600

    Agregar archivos de proyecto.

---
+++ Example/ViewModels/MainViewModel.cs
@@ -0,0 +1,92 @@
+﻿using Example.ViewModels.Functions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Example.ViewModels
+{
+    public class MainViewModel :INotifyPropertyChanged
+    {
+
+        public ICommand MinimizeCommand { get; }
+        public ICommand MaximizeCommand { get; }
+        public ICommand CloseCommand { get; }
+        
+
+        private object? _currentView;
+
+        public object? CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value; 
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        public MainViewModel()
+        {
+            CurrentView = new FirstScreenViewModel();
+
+            MinimizeCommand = new RelayCommand(MinimizeWindow);
+            MaximizeCommand = new RelayCommand(MaximizeWindow);
+            CloseCommand = new RelayCommand(CloseWindow);
+        }
+
+
+        /// <summary>
+        /// WINDOW FUNCTIONS
+        /// </summary>
+        
+
+        private void MinimizeWindow()
+        {
+            Application.Current.MainWindow.WindowState=WindowState.Minimized;
+        }
+
+        private void MaximizeWindow()
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+
+
+        }
+
+        private void CloseWindow()
+        {
+            Application.Current.Shutdown();
+        }
+
+
+
+
+
+        /// <summary>
+        /// VIEW FUNCTIONS
+        /// </summary>
+
+
+        public void ShowSecondScreen()
+        {
+            CurrentView = new SecondScreenViewModel();
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+    }
+}
